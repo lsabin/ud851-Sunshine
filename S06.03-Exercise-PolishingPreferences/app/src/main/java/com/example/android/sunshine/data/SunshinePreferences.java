caab954c9a164d516bf2e2ac0981746e5457919e
lsabin <sabin.leandro@gmail.com>
2017-12-28 09:13:52 +0100

    Lesson 7 - Exercise 3 , Polishing Preferences

--- S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
+++ S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
@@ -16,6 +16,10 @@
 package com.example.android.sunshine.data;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.preference.PreferenceManager;
+
+import com.example.android.sunshine.R;
 
 public class SunshinePreferences {
 
@@ -87,9 +91,10 @@ public class SunshinePreferences {
      * "94043,USA" if SharedPreferences have not been implemented yet.
      */
     public static String getPreferredWeatherLocation(Context context) {
-        // TODO (1) Return the user's preferred location
-        /** This will be implemented in a future lesson **/
-        return getDefaultWeatherLocation();
+        // (1) Return the user's preferred location
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getString(context.getString(R.string.pref_location_key),
+                context.getString(R.string.pref_location_default));
     }
 
     /**
@@ -100,9 +105,15 @@ public class SunshinePreferences {
      * @return true If metric display should be used
      */
     public static boolean isMetric(Context context) {
-        // TODO (2) Return true if the user's preference for units is metric, false otherwise
-        /** This will be implemented in a future lesson **/
-        return true;
+        // (2) Return true if the user's preference for units is metric, false otherwise
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String unitsPrefernce = preferences.getString(context.getString(R.string.pref_units_key),
+                context.getString(R.string.pref_units_metric));
+
+        String metricUnitsLabel = context.getString(R.string.pref_units_metric);
+
+
+        return metricUnitsLabel.equals(unitsPrefernce);
     }
 
     /**
